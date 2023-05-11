1f32274e8821ea20726312c0f80fcacfd5be2ae0
ShradhaGupta26 <88206733+ShradhaGupta26@users.noreply.github.com>
2023-05-11 14:35:54 +0530

    Update user_data.sh
    added script for node-exporter

--- user_data.sh
+++ user_data.sh
@@ -18,3 +18,41 @@ echo "xpack.security.enabled: false"  >> /etc/elasticsearch/elasticsearch.yml
 echo "xpack.security.transport.ssl.enabled: false"  >> /etc/elasticsearch/elasticsearch.yml
 
 sudo systemctl restart elasticsearch.service
+
+#node-exporter script
+Node exporter
+
+ #!/bin/bash
+sudo yum update -y
+wget https://github.com/prometheus/node_exporter/releases/download/v1.2.0/node_exporter-1.2.0.linux-amd64.tar.gz
+tar xvfz node_exporter-1.2.0.linux-amd64.tar.gz
+sudo mv node_exporter-1.2.0.linux-amd64/node_exporter /usr/local/bin/
+
+sudo tee /etc/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=default.target
+EOF
+
+
+sudo useradd -rs /bin/false node_exporter
+
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+sudo chmod 755 /usr/local/bin/node_exporter
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+
+sudo systemctl status node_exporter
